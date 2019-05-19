cc4a1124c43c3a2bc72d7da873e6d3736337c2e9
vlearner <barotvishal12@outlook.com>
2019-05-18 23:49:31 -0500

    2nd commmit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.*;
 
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,19 +14,29 @@ public class Main {
 
         do {
             System.out.println("What would you like to do?");
-            System.out.println("View all pets");
-            System.out.println("Add more pets");
-            System.out.println("Update an existing pet");
-            System.out.println("Remove an existing pet");
-            System.out.println("Search pets by name");
-            System.out.println("Search pets by age");
-            System.out.println("Exit program");
+            System.out.println("1. View all pets");
+            System.out.println("2. Add more pets");
+            System.out.println("3. Update an existing pet");
+            System.out.println("4. Remove an existing pet");
+            System.out.println("5. Search pets by name");
+            System.out.println("6. Search pets by age");
+            System.out.println("7. Exit program");
             System.out.print("Your choice: ");
             userInput = s.nextInt();
 
 
             if (userInput == 1) {
                 System.out.println("1");
+            }
+            if(userInput == 2){
+                while (true){
+
+                System.out.print("\nadd pet (name, age):");
+                String petInfo = s.next();
+                System.out.print("Type exit to go main menu");
+                if ( petInfo.equals("Exit") | petInfo.equals("exit")){ break;}
+
+                }
             }
 
         }while(userInput != 7);
