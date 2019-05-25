51adb37aded6a0c4212d748a38677c309257216c
vlearner <barotvishal12@outlook.com>
2019-05-24 22:36:43 -0500

    Week 2 Milestone 1

--- src/Main.java
+++ src/Main.java
@@ -12,49 +12,41 @@ public class Main {
         System.out.println();
 
 
-        do {
+        while (true){
             System.out.println("What would you like to do?");
             System.out.println("1. View all pets");
-            System.out.println("2. Add more pets");
-            System.out.println("3. Update an existing pet");
-            System.out.println("4. Remove an existing pet");
-            System.out.println("5. Search pets by name");
-            System.out.println("6. Search pets by age");
-            System.out.println("7. Exit program");
+            System.out.println("2. Add new pets");
+            System.out.println("3. Remove a pet");
+            System.out.println("4. Exit program");
             System.out.print("Your choice: ");
             userInput = s.nextInt();
 
 
             if (userInput == 1) {
-                System.out.println("1");
+                System.out.println("\n1 do something\n");
             }
-            if(userInput == 2){
+            else if (userInput == 2){
                 while (true){
-
-                System.out.print("\nadd pet (name, age):");
+                System.out.print("Type exit to go main menu\n");
+                System.out.print("add pet (name, age):");
                 String petInfo = s.next();
-                System.out.print("Type exit to go main menu");
+
+
                 if ( petInfo.equals("Exit") | petInfo.equals("exit")){ break;}
 
                 }
             }
+            else if (userInput == 3){
+                System.out.println("\n3 do something\n");
+            }
+            else {
+                System.out.println("Done");
 
-        }while(userInput != 7);
+                System.exit(0);
+            }
 
-          if(userInput == 7) {
-              System.out.println("Done");
+        }
 
-              System.exit(0);
-          }
-
-
-
-//        Scanner petName = new Scanner(System.in);
-//        System.out.println("Enter Pet Name: ");
-//        String name = petName.next();
-//        System.out.println(name);
-
-//        petName.close();
 
 
     }
